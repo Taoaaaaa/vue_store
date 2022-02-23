b39a94827faa6290ffa0a0d1901eeee58132d49b
Taoaaaaa <215763834@qq.com>
2022-02-23 20:25:53 +0800

    提交文件

---
+++ src/store/index.js
@@ -0,0 +1,22 @@
+/*
+ * @Description: Vuex入口
+ * @Author: Tao
+ * @Date: 2022-02-07 16:23:00
+ * @LastEditors: Tao
+ * @LastEditTime: 2022-02-25 22:51:50
+ */
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import user from './modules/user'
+import shoppingCart from './modules/shoppingCart'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  strict: true,
+  modules: {
+    user,
+    shoppingCart
+  }
+})
